6d37bc07aa23a367e0d6bb49459e16513a74609b
Jakyle <jjackson360@gmail.com>
2023-05-18 20:42:59 -0400

    consistent host bindings for toasts

--- src/app/toast-overlay/toast-item/toast-item.component.ts
+++ src/app/toast-overlay/toast-item/toast-item.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Subject, takeUntil, timer } from 'rxjs';
-import { Toast } from 'src/app/toast';
+import { Toast, ToastType } from 'src/app/toast';
 
 @Component({
   selector: 'app-toast-item',
@@ -11,6 +11,9 @@ export class ToastItemComponent implements OnInit, OnDestroy {
   protected readonly destroyed: Subject<void> = new Subject();
   @Input({ required: true }) toast!: Toast;
   @Output() close = new EventEmitter<number>();
+
+  @HostBinding('class') class = 'min-h-fit shadow px-4 py-6 rounded text-white flex flex-col data-info:bg-sky-600 data-success:bg-green-600 data-danger:bg-red-600 data-warning:bg-yellow-600';
+  @HostBinding('attr.data-type') get type() { return this.toast.type; };
 
   onClose() {
     this.close.emit(this.toast.id);
